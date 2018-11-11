8942714796f8cefcc1731bff4418774804d96d8b
Felipe Soares <felipeluizsoares@gmail.com>
2018-11-11 14:56:58 -0200

    Update app to bind the prisma database

--- src/index.js
+++ src/index.js
@@ -1,49 +1,20 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } =  require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
-const links = [
-  {
-    id: '1',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-]
-
-const users = [
-  {
-    id: '1',
-    name: 'Felipe',
-    email: 'felipe@son.com'
-  }
-]
-
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    users: () => users,
-    user: (root, { id }) => {
-      return users.find(user => user.id === id)
-    }
-  },
-
-  Mutation: {
-    post(root, args) {
-      const link = {
-        id: links.length + 1,
-        description: args.description,
-        url: args.url,
-      }
-
-      links.push(link)
-
-      return link
-    }
-  }
-}
+const resolvers = require('./resolvers')
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/felipe-luiz-soares-633aab/graphql-node-demo/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  }),
 })
 
 server.start(() => console.log('Server is running on localhost:4000'))
